343763d1ba84f69eac05c76ad45a49dbfd5886e9
Dimitrios Nikitopoulos <diminiki@Dimitrioss-MBP.lan>
2019-08-09 17:28:22 +0300

    added topbar profile footer components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import TopBar from './components/TopBar';
+import Profile from './components/Profile';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <TopBar />
+          <Profile />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
